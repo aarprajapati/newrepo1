8fdb2d01e00ebca15d039cc15d661e93374019fa
Rajesh Prajapati <aarprajapati3@gmail.com>
2025-09-19 00:28:26 +0530

    checking my github

--- main.tf
+++ main.tf
@@ -6,17 +6,11 @@ terraform {
 
     }
 }
-backend "azurerm" {
-#   resource_group_name = azurerm_resource_group.rjpipe1.name
-#   storage_account_name = "rjpipe1storage"
-#   container_name       = "tfstate"
-#   key                  = "terraform.tfstate"
-  
-}
+
 }
 provider "azurerm" {
   features {}
-  subscription_id = "2c279754-9ded-4271-a952-14fe81a08ff4"
+  subscription_id = "787c86fa-2cad-40aa-8646-4356806d5a43"
 }
 resource "azurerm_resource_group" "rjpipe5" {
   name     = "rjpipe_rg5"
